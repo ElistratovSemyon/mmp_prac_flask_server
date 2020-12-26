b26a7fea9c5d9ca0849e4658c39b3414136f7573
Семён Елистратов <semenelist@gmail.com>
2020-12-26 22:55:52 +0400

    add cleaner

--- src/run.py
+++ src/run.py
@@ -1,5 +1,9 @@
+import os
 from ml_server import app
 
 if __name__ == '__main__':
     s = app.run(host='0.0.0.0', port=5000, debug=True)
-    print(s)
+    folder = os.listdir("./")
+    for item in folder:
+        if item.endswith(".pkl"):
+            os.remove(os.path.join("./", item))
